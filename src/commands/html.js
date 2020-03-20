b8ad98b660463c7af61128b09f078e1acafc6555
EduardoGomes18 <eduardo_gomes1318@outlook.com>
2020-03-20 15:14:58 -0300

    sheldon v2

--- src/commands/html.js
+++ src/commands/html.js
@@ -1,21 +1,53 @@
 const command = {
-  name: 'html',
+  name: "html",
   run: async toolbox => {
-    const { print, parameters } = toolbox
-    let fs = require('fs')
+    const { print, parameters } = toolbox;
+    let fs = require("fs");
 
-    fs.readFile('nov.html', 'utf8', function(err, data) {
-      let result = data
-      let teste = JSON.stringify(result)
+    fs.readFile("nov.html", "utf8", function(err, data) {
+      let result = data;
+      let teste = JSON.stringify(result);
 
       let splice = teste.replace(
         /<tr>/g,
         '<table border="0" cellpadding="0" cellspacing="0"> <tr>'
-      )
-      let splice2 = splice.replace(/<\/tr>/g, '</tr> </table>')
-      fs.writeFileSync('index.html', splice2)
-    })
-  }
-}
+      );
+      let splice2 = splice.replace(/<\/tr>/g, "</tr> </table>");
 
-module.exports = command
+      let splice3 = splice2.replace(/\\r/g, "");
+
+      let splice4 = splice3.replace(/\\n/g, "");
+
+      let splice5 = splice4.replace(/\\t/g, "");
+
+      let splice6 = splice5.replace(/.$/, "");
+
+      let splice7 = splice6.replace(/"/, "");
+
+      let splice8 = splice7.replace(/\\/g, "");
+
+      let splice9 = splice8.replace(
+        /\alt=""/g,
+        'style="display:block; margin:0; padding:0;"'
+      );
+
+      let splice10 = splice9.replace(
+        /<table border="0" cellpadding="0" cellspacing="0">/,
+        '<tr> <td> <table border="0" cellpadding="0" cellspacing="0">'
+      );
+
+      let splice11 = splice10.replace(
+        /<\/table><!-- End Save for Web Slices -->/,
+        "</td> </tr> </table>"
+      );
+
+      let spliceLast = splice11.replace(/images\//g, "");
+
+      console.log(spliceLast);
+
+      // fs.writeFileSync("index.html", splice11);
+    });
+  }
+};
+
+module.exports = command;
